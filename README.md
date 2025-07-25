03689c40c496ce8f21bcf7c7d78932e7a58946e8
SRINU NAYAK <srinunayakk7@gmail.com>
2025-07-25 15:04:26 -0400

    added template file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # srinu_mlops_template
 creating a template for all kind of mlops projects
+
+1. conda create -p venv/ python==3.10
+2. conda activate venv/
+3. template.py
+4. 
---
+++ template.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "mlproject"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_monitor.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/training_pipeline.py",
+    f"src/{project_name}/pipeline/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/utils.py",
+    f"src/{project_name}/logger.py",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"created file directory {filedir}")
+
+    if (not os.path.exists(filepath) or os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"created file {filepath}")
+
+    else:
+        logging.info(f"file {filename} already exists")
